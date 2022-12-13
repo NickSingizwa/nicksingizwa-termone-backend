418b80d474497a4ee235d581376e6fe26c5fb41d
NickSingizwa <singizwanick19@gmail.com>
2022-12-13 11:37:33 +0200

    updating MathServiceTest

--- src/test/java/rw/rca/calculatorapp/unit/MathServiceTest.java
+++ src/test/java/rw/rca/calculatorapp/unit/MathServiceTest.java
@@ -21,49 +21,49 @@ public class MathServiceTest {
 
 
     @Test
-    public void should_return_number_on_addition_operation_success(){
+    public void return_number_on_addition_success(){
         double actualResult = mathService.doMath(20.0,2.0,"+");
         double expectedResults = 22.0;
         assertEquals(actualResult,expectedResults);
     }
 
     @Test
-    public void should_return_number_on_subtraction_operation_success(){
+    public void return_number_on_subtraction_success(){
         double actualResult = mathService.doMath(20.0,2.0,"-");
         double expectedResults = 18.0;
         assertEquals(actualResult,expectedResults);
     }
 
     @Test
-    public void should_return_number_on_multiplication_operation_success(){
+    public void return_number_on_multiplication_success(){
         double actualResult = mathService.doMath(20.0,2.0,"*");
         double expectedResults = 40.0;
         assertEquals(actualResult,expectedResults);
     }
 
     @Test
-    public void should_return_number_on_division_operation_success(){
+    public void return_number_on_division_success(){
         double actualResult = mathService.doMath(20.0,2.0,"/");
         double expectedResults = 10.0;
         assertEquals(actualResult,expectedResults);
     }
 
     @Test
-    public void should_return_number_on_power_operation_success(){
+    public void return_number_on_power_success(){
         double actualResult = mathService.doMath(20.0,2.0,"**");
         double expectedResults = 400.0;
         assertEquals(actualResult,expectedResults);
     }
 
     @Test
-    public void should_return_number_on_log_operation_success(){
+    public void _return_number_on_log_success(){
         double actualResult = mathService.doMath(20.0,2.0,"log");
         double expectedResults = 6.020599913279624;
         assertEquals(actualResult,expectedResults);
     }
 
     @Test
-    public void should_return_number_on_ln_operation_success(){
+    public void return_number_on_ln_success(){
         double actualResult = mathService.doMath(20.0,2.0,"ln");
         double expectedResults = 13.862943611198906;
         assertEquals(actualResult,expectedResults);
